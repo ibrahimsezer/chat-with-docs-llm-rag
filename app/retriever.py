23067b6e2366515c2e6f0f20038bfb5cc425c9ed
İbrahim SEZER <ibrahimszr16@gmail.com>
2025-06-22 13:48:21 +0300

    Enhance retriever functionality by adding document ID support for chunk storage and retrieval. Implement UUID generation for unique chunk identifiers and improve error handling and logging during data addition and query processes.

--- app/retriever.py
+++ app/retriever.py
@@ -1,33 +1,49 @@
 import chromadb
+from chromadb import Client
 from chromadb.config import Settings
-
 from app.embedding import fake_embed
+import uuid
 
-chroma_client = chromadb.PersistentClient(path="./data/chroma")
+chroma_client = Client(Settings(
+    persist_directory="./data/chroma",  
+    anonymized_telemetry=False
+))
 collection = chroma_client.get_or_create_collection(name="documents")
 
-def add_to_vector_store(chunks: list[str]):
+def add_to_vector_store(chunks: list[str], doc_id: str):
+    print(f"{len(chunks)} adet chunk '{doc_id}' ID'siyle ekleniyor...")
+    
     for i, chunk in enumerate(chunks):
-        emb = fake_embed(chunk)  # sonra gerçek embedding fonksiyonuyla değiştir
-        collection.add(
-            documents=[chunk],
-            ids=[f"chunk_{i}"],
-            embeddings=[emb]
-        )
+        try:
+            unique_id = f"{doc_id}_chunk_{i}_{uuid.uuid4().hex}"
+            emb = fake_embed(chunk)
+            collection.add(
+                documents=[chunk],
+                ids=[unique_id],
+                embeddings=[emb],
+                metadatas=[{"doc_id": doc_id}],
+            )
+            print(f"{i+1}. chunk eklendi: {unique_id}")
+        except Exception as e:
+            print(f"[HATA] {i+1}. chunk eklenemedi: {e}")
+    
+    print("Veriler ChromaDB'ye eklendi.")
 
 
-def get_relevant_chunks(query: str, top_k: int = 3) -> list[str]:
+def get_relevant_chunks(query: str, doc_id:str, top_k: int = 3) -> list[str]:
     query_embedding = fake_embed(query)
     print(f"Embedding alındı: {query_embedding[:5]}...")
 
     try:
         results = collection.query(
             query_embeddings=[query_embedding],
-            n_results=top_k
+            n_results=top_k,
+            where={"doc_id":doc_id}
         )
         print("Query sonucu alındı:", results)
+        
     except Exception as e:
         print("[HATA] query sırasında hata:", e)
         return []
 
-    return results["documents"][0]
+    return results["documents"][0] if results["documents"] else []
